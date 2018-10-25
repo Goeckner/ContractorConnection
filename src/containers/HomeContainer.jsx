925d58464840ddf04544ee33351b793b889ea28d
Justin Goeckner <jagoec@monsanto.com>
2018-10-25 16:23:36 -0500

    Formatting

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -3,19 +3,22 @@ import { simpleAction } from '../redux/actions/simpleAction'
 import React from 'react'
 import logo from '../logo.svg'
 
-const HomeContainer = props => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <h1 className="App-title">Welcome to Contractor Connection</h1>
-    </header>
-    <button onClick={props.simpleAction}>
-      Test redux action
-    </button>
-    <div>
-      {props.simpleReducer}
+const HomeContainer = ({
+  simpleReducer,
+  simpleAction,
+  }) => (
+    <div className="App">
+      <header className="App-header">
+        <img alt="logo" className="App-logo" src={logo} />
+        <h1 className="App-title">Welcome to Contractor Connection</h1>
+      </header>
+      <button onClick={simpleAction}>
+        Test redux action
+      </button>
+      <div>
+        {simpleReducer}
+      </div>
     </div>
-  </div>
 )
 
 const mapStateToProps = state => ({
