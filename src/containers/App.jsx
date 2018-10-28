fc5b75fa40c45cb8e7200ca8221b5d19f3588152
Justin Goeckner <jagoec@monsanto.com>
2018-10-28 16:53:17 -0500

    Pulled the buttons to the app container

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,8 @@
 import '../App.css'
 import { Route, Switch } from 'react-router-dom'
+import { Panel } from 'react-bootstrap'
 import { connect } from 'react-redux'
+import { setActivePage } from '../redux/actions/rootActions'
 import HomeContainer from './HomeContainer'
 import InstructorsContainer from './InstructorsContainer'
 import React, { Component } from 'react'
@@ -8,10 +10,25 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route component={HomeContainer} exact path="/" />
-        <Route component={InstructorsContainer} exact path="/instructors" />
-      </Switch>
+      <div>
+        <Panel>
+          <Panel.Heading>
+            <button onClick={() => this.props.setActivePage('instructors')}>
+              Instructor Page
+            </button>
+            <button onClick={() => this.props.setActivePage('home')}>
+              Home Page
+            </button>
+          </Panel.Heading>
+          <Panel.Body>
+            {this.props.activePage}
+          </Panel.Body>
+        </Panel>
+        <Switch>
+          <Route component={HomeContainer} exact path="/" />
+          <Route component={InstructorsContainer} exact path="/instructors" />
+        </Switch>
+      </div>
     )
   }
 }
@@ -21,7 +38,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-
+  setActivePage: page => dispatch(setActivePage(page)),
 })
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps)
--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,8 +1,6 @@
 import { connect } from 'react-redux'
-import { setActivePage } from '../redux/actions/rootActions'
 import React from 'react'
 import logo from '../logo.svg'
-import { Panel } from 'react-bootstrap'
 
 const HomeContainer = props => (
     <div className="App">
@@ -10,19 +8,6 @@ const HomeContainer = props => (
         <img alt="logo" className="App-logo" src={logo} />
         <h1 className="App-title">Welcome to Contractor Connection</h1>
       </header>
-      <Panel>
-        <Panel.Heading>
-          <button onClick={() => props.setActivePage('instructors')}>
-            Instructor Page
-          </button>
-          <button onClick={() => props.setActivePage('home')}>
-            Home Page
-          </button>
-        </Panel.Heading>
-        <Panel.Body>
-          {props.activePage}
-        </Panel.Body>
-      </Panel>
     </div>
 )
 
@@ -31,7 +16,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  setActivePage: page => dispatch(setActivePage(page)),
+
 })
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps)
