fb0cef04d53ced92fd6262217e0199f5a66674b2
Jacob <jacobr8893@gmail.com>
2018-10-28 18:21:15 -0500

    Changed SearchContainer to use a bootstrap grid

--- src/containers/SearchContainer.jsx
+++ src/containers/SearchContainer.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Grid,Clearfix,Col,Row } from 'react-bootstrap';
 import SearchBar from '../components/Search-Components/Search-BarComponent'//Search-BarComponent from '../components/Search-Components/Search-BarComponent'
 import InstructorsList from '../components/Search-Components/Instructors-ListComponent'
 
 const SearchContainer = props => (
-  <div className="App">
-    <SearchBar />
-    <InstructorsList />
-  </div>
+	<Grid className="instructor-search-container" >
+
+		<Row className="search-utils" >
+			<SearchBar />
+		</Row >
+
+		<Row className="results-container" >
+
+			<Col className="results-list-container" >
+				<InstructorsList />
+			</Col >
+
+			<Col className="results-map-container" >
+			</Col >
+
+		</Row >
+		
+	</Grid >
 )
 
 const mapStateToProps = state => ({
