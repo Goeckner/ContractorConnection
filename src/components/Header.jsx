c253395abbde78d9eef7406d065fa7801980a90f
Justin Goeckner <jagoec@monsanto.com>
2018-10-28 18:35:09 -0500

    Started styling the navbar (semi mobile responsive)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,31 @@
-import { Panel } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { setActivePage } from '../redux/actions/rootActions'
 import { Link } from 'react-router-dom'
 import React from 'react'
 
 const Header = props => (
-  <Panel>
-    <Panel.Heading>
-      <Link to="/instructors">
-        <button onClick={() => props.setActivePage('instructors')}>
-          Instructor Page
-        </button>
-      </Link>
-      <Link to="/">
-        <button onClick={() => props.setActivePage('home')}>
-          Home
-        </button>
-      </Link>
-    </Panel.Heading>
-  </Panel>
+  <header>
+    <Navbar className="row">
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">
+              Home
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <Navbar.Brand>
+            <Link to="/instructors">
+              Instructor Page
+            </Link>
+          </Navbar.Brand>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </header>
 )
 
 const mapStateToProps = state => ({
