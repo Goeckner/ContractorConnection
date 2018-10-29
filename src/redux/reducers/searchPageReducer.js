cbbd2c4c9ad518c04a9af5b56fb83a8bbff0f6ae
Brady Smith <bradsmi@siue.edu>
2018-10-28 19:02:58 -0500

    uncommented a section of a reducer

--- src/redux/reducers/searchPageReducer.js
+++ src/redux/reducers/searchPageReducer.js
@@ -11,14 +11,14 @@ const instructors = [
     {name: 'John Smith', email: 'Example@fake.com', location: 'Edwardsville', company: 'Guns r Us'}
 ]
 
-// export const instructorList = (state = instructors, action) => {
-//     switch ( action.type ) {
-//       case 'SET_INSTRUCTOR_LIST':
-//         return action.payload
-//       default:
-//         return state
-//     }
-//   }
+export const instructorList = (state = instructors, action) => {
+    switch ( action.type ) {
+      case 'SET_INSTRUCTOR_LIST':
+        return action.payload
+      default:
+        return state
+    }
+  }
   
   export default function (state = null, action) {
     return instructors
