cccfe44806bf02592ca82972da9686ad48483808
Justin Goeckner <jagoec@monsanto.com>
2018-10-30 16:23:19 -0500

    Added a basic login form to the login screen

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { ControlLabel, Form, FormGroup, Col, Button, FormControl, Checkbox } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const LoginContainer = props => (
-	<div>
-    Login Screen
-  </div>
+	<Form horizontal className="loginForm">
+    <FormGroup controlId="formHorizontalEmail">
+      <Col componentClass={ControlLabel} sm={2}>
+        Email
+      </Col>
+      <Col sm={10}>
+        <FormControl type="email" placeholder="Email" />
+      </Col>
+    </FormGroup>
+
+    <FormGroup controlId="formHorizontalPassword">
+      <Col componentClass={ControlLabel} sm={2}>
+        Password
+      </Col>
+      <Col sm={10}>
+        <FormControl type="password" placeholder="Password" />
+      </Col>
+    </FormGroup>
+
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Checkbox>Remember me</Checkbox>
+      </Col>
+    </FormGroup>
+
+    <FormGroup>
+      <Col smOffset={2} sm={2}>
+        <Button bsStyle="primary" type="submit">Sign in</Button>
+      </Col>
+      <Col sm={2}>
+        <Link to="/sign-up">
+          <Button type="submit">Register</Button>
+        </Link>
+      </Col>
+    </FormGroup>
+  </Form>
 )
 
 const mapStateToProps = state => ({
