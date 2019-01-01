f9387e8fcb69eab7729745ba1f03a528f93c8272
Brady Smith <bradsmi@siue.edu>
2019-01-01 15:51:12 -0600

    Added a little bit to the Search filter. At the moment the text box in the dropdown closes it, so that needs to be fixed

--- src/components/Search-Components/Search-FilterComponent.jsx
+++ src/components/Search-Components/Search-FilterComponent.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import map from 'lodash/map'
-import {DropdownButton, ButtonGroup, Col, Row} from 'react-bootstrap'
+import {FormGroup, FormControl, DropdownButton, Dropdown, ButtonGroup, Col, Row} from 'react-bootstrap'
 
 const FilterDropDown = props => {
     return (
-        <div>
+        <div> 
             <Row className = "filter-title">
                 Location:
             </Row>
-            <Row>
-                
-            </Row>
+                <Row className = "filter-sub-title">
+                    Distance
+                </Row>
+                    <Row className = "filter-regular">
+                        Maximum Distance in Miles
+                    </Row>
+                    <FormGroup className = "filter-regular">
+                    <FormControl componentClass="select" placeholder="No Max">
+                        <option value="No Max">No Max</option>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </FormControl>
+                    </FormGroup>
+                    <Row className = "filter-regular">
+                        From
+                    </Row>
+                    <FormGroup className = "filter-regular">
+                    <FormControl onClick = {(e) => {e.preventDefault()
+                                                    e.stopPropagation()
+                                                    document.getElementById("filter-dropdown").setAttribute("aria-expanded", "true")
+                        }} className = "filter-regular" type="text" placeholder="Address" />
+                    </FormGroup>                    
             <Row className = "filter-title">
                 Rating:
             </Row>
@@ -23,9 +45,10 @@ const FilterDropDown = props => {
 const SearchFilter = props => {
     return (
         <ButtonGroup justified>
-            <DropdownButton            
+            <DropdownButton           
                 bsSize="large"
                 title = "Advanced Search"
+                id = "filter-dropdown"
             >
                 <Col className = "filter-selection">
                     <FilterDropDown />
--- src/redux/reducers/searchPageReducer.js
+++ src/redux/reducers/searchPageReducer.js
@@ -1,26 +1,26 @@
 import { combineReducers } from "redux";
 
 const instructors = [
-    {name: 'Dennis Ritchie', email: 'DRitchie@unix.c', location: 'Boston', company: 'Guns r Us', lat: 42.3600825, lng: -71.05888801},
-    {name: 'Penny Hanson', email: 'Example@fake.com', location: 'Central Park, New York', company: 'Guns r Us', lat: 40.7828647, lng: -73.9653551},
-    {name: 'John Smith', email: 'maybe@google.com', location: '770 Puma Blvd', company: 'Walmart', lat: 38.8029004, lng: -90.0110577},
-    {name: 'Mary johnson', email: 'Example@comcast.com', location: 'White House', company: 'Guns r Us', lat: 38.8976763, lng: -77.0365298},
-    {name: 'Michael Morgan', email: 'Example@fake.org', location: 'Busch Stadium', company: 'Guns r Us', lat: 38.6226188, lng: -90.1928209},
-    {name: 'John Smith', email: 'brad@real.com', location: '61 Circle Drive Edwardsville', company: 'Gap', lat: 38.7925599, lng: -90.00304190000001},
-    {name: 'Abbie Smith', email: 'kim@tim.com', location: 'Glen Carbon', company: 'Apple', lat: 38.7483814, lng: -89.9831579999999},
-    {name: 'Michael Patrick', email: 'Landon@fake.com', location: 'Troy, Illinois', company: 'Guns r Us', lat: 38.7292147, lng: -89.8831541},
-    {name: 'Jim Jones', email: 'me@siue.com', location: 'New York', company: 'Guns r Us', lat: 40.7127753, lng: -74.0059728},
-    {name: 'John Kelly', email: 'Kelly@outlook.com', location: 'Austin, Texas', company: 'Guns r Us', lat: 30.267153, lng: -97.7430608},
-    {name: 'James Bond', email: 'JBond@MI6.uk', location: 'London', company: 'MI6', lat: 51.5073509, lng: -0.1277583},
-    {name: 'Patrick Brown', email: 'PBrown@fbi.gov', location: 'Pentagon', company: 'CIA', lat: 38.8718568, lng: -77.0562669},
-    {name: 'Tyler Richards', email: 'TRich@yahoo.com', location: 'Los Angeles', company: 'The Dodgers', lat: 34.0522342, lng: -118.2436849},
-    {name: 'Michael Morrison', email: 'me@mm.com', location: 'Washington Monument', company: 'NRA', lat: 38.8894838, lng: -77.0352791},
-    {name: 'Samuel Adams', email: 'freedom@usa.gov', location: 'St. Louis', company: 'Founding Fathers', lat: 38.6270025, lng: -90.19940419999999},
-    {name: 'John Adams', email: 'ja@real.com', location: '61 Circle Drive Edwardsville', company: 'Gap', lat: 38.7925599, lng: -90.00304190000001},
-    {name: 'Ronald Swanson', email: 'null@null.org', location: 'Pawnee, Illinois', company: 'Parks and Recreation', lat: 39.5917186, lng: -89.58037390000001},
-    {name: 'Michael Scott', email: 'MScott@DunderMifflin.com', location: 'Scranton, Pennsylvania', company: 'Guns r Us', lat: 41.408969, lng: -75.66241219999999},
-    {name: 'Bill Gates', email: 'BGates@windows.com', location: 'Seattle, Washington', company: 'Microsoft', lat: 47.6062095, lng: -122.3320708},
-    {name: 'Steve Jobs', email: 'sj@apple.com', location: 'Seattle, Washington', company: 'Apple', lat: 47.6062095, lng: -122.3320708}
+    {id: 1, name: 'Dennis Ritchie', email: 'DRitchie@unix.c', location: 'Boston', company: 'Guns r Us', lat: 42.3600825, lng: -71.05888801},
+    {id: 2, name: 'Penny Hanson', email: 'Example@fake.com', location: 'Central Park, New York', company: 'Guns r Us', lat: 40.7828647, lng: -73.9653551},
+    {id: 3, name: 'John Smith', email: 'maybe@google.com', location: '770 Puma Blvd', company: 'Walmart', lat: 38.8029004, lng: -90.0110577},
+    {id: 4, name: 'Mary johnson', email: 'Example@comcast.com', location: 'White House', company: 'Guns r Us', lat: 38.8976763, lng: -77.0365298},
+    {id: 5, name: 'Michael Morgan', email: 'Example@fake.org', location: 'Busch Stadium', company: 'Guns r Us', lat: 38.6226188, lng: -90.1928209},
+    {id: 6, name: 'John Smith', email: 'brad@real.com', location: '61 Circle Drive Edwardsville', company: 'Gap', lat: 38.7925599, lng: -90.00304190000001},
+    {id: 7, name: 'Abbie Smith', email: 'kim@tim.com', location: 'Glen Carbon', company: 'Apple', lat: 38.7483814, lng: -89.9831579999999},
+    {id: 8, name: 'Michael Patrick', email: 'Landon@fake.com', location: 'Troy, Illinois', company: 'Guns r Us', lat: 38.7292147, lng: -89.8831541},
+    {id: 9, name: 'Jim Jones', email: 'me@siue.com', location: 'New York', company: 'Guns r Us', lat: 40.7127753, lng: -74.0059728},
+    {id: 10, name: 'John Kelly', email: 'Kelly@outlook.com', location: 'Austin, Texas', company: 'Guns r Us', lat: 30.267153, lng: -97.7430608},
+    {id: 11, name: 'James Bond', email: 'JBond@MI6.uk', location: 'London', company: 'MI6', lat: 51.5073509, lng: -0.1277583},
+    {id: 12, name: 'Patrick Brown', email: 'PBrown@fbi.gov', location: 'Pentagon', company: 'CIA', lat: 38.8718568, lng: -77.0562669},
+    {id: 13, name: 'Tyler Richards', email: 'TRich@yahoo.com', location: 'Los Angeles', company: 'The Dodgers', lat: 34.0522342, lng: -118.2436849},
+    {id: 14, name: 'Michael Morrison', email: 'me@mm.com', location: 'Washington Monument', company: 'NRA', lat: 38.8894838, lng: -77.0352791},
+    {id: 15, name: 'Samuel Adams', email: 'freedom@usa.gov', location: 'St. Louis', company: 'Founding Fathers', lat: 38.6270025, lng: -90.19940419999999},
+    {id: 16, name: 'John Adams', email: 'ja@real.com', location: '61 Circle Drive Edwardsville', company: 'Gap', lat: 38.7925599, lng: -90.00304190000001},
+    {id: 17, name: 'Ronald Swanson', email: 'null@null.org', location: 'Pawnee, Illinois', company: 'Parks and Recreation', lat: 39.5917186, lng: -89.58037390000001},
+    {id: 18, name: 'Michael Scott', email: 'MScott@DunderMifflin.com', location: 'Scranton, Pennsylvania', company: 'Guns r Us', lat: 41.408969, lng: -75.66241219999999},
+    {id: 19, name: 'Bill Gates', email: 'BGates@windows.com', location: 'Seattle, Washington', company: 'Microsoft', lat: 47.6062095, lng: -122.3320708},
+    {id: 20, name: 'Steve Jobs', email: 'sj@apple.com', location: 'Seattle, Washington', company: 'Apple', lat: 47.6062095, lng: -122.3320708}
 ]
 
 var defaultCoordinates = {
