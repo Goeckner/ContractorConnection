89add41c8808a39de15cc2becd7d49c761a4681c
Billy Rohrkaste <williamrohrkaste@gmail.com>
2019-02-13 15:41:03 -0600

    Training page so far (still needs work)

--- src/containers/TrainingPage.jsx
+++ src/containers/TrainingPage.jsx
@@ -1,9 +1,30 @@
 import { connect } from 'react-redux'
 import React from 'react'
+import ReactPlayer from 'react-player'
+import { Form, FormGroup, Col, Radio, Button } from 'react-bootstrap'
+import quizData from '../questions'
 
 const TrainingPage = props => (
   <div>
-    This is the training page!
+    <h2 className="quiz">Diversity Training Video</h2>
+    <ReactPlayer url='https://youtu.be/UW28j_V3dSI' className="quiz"/>
+
+  <Form>
+    <FormGroup>
+      {quizData[0].question}
+      <Radio name="groupOptions"><h4>{quizData[0].answers[0]}</h4></Radio>
+      <Radio name="groupOptions"><h4>{quizData[0].answers[1]}</h4></Radio>
+      <Radio name="groupOptions"><h4>{quizData[0].answers[2]}</h4></Radio>
+      <Radio name="groupOptions"><h4>{quizData[0].answers[3]}</h4></Radio>
+    </FormGroup>
+
+    <FormGroup >
+      <Col smOffset={2} sm={2}>
+        <Button bsStyle="primary" type="submit">Submit answers</Button>
+      </Col>
+    </FormGroup>
+  </Form>
+
   </div>
 )
 
---
+++ src/questions.js
@@ -0,0 +1,24 @@
+var quizData = [
+    {
+      question: "What is diversity?",
+      answers: [
+        "this",
+        "that",
+        "this and that",
+        "none"
+      ],
+      correct: 1
+    },
+    {
+      question: "Question 2?",
+          answers: [
+            "asdf",
+            "asdf",
+            "asdf",
+            "asdf"
+          ],
+          correct: 3
+    }
+  ];
+
+  export default quizData;
