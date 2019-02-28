75833a74663ac0db567e96d537c9728ad32baf0e
Brady Smith <bradsmi@siue.edu>
2019-02-27 23:05:08 -0600

    saving to switch computers

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -84,42 +84,6 @@ const LoginContainer = props => {
     </Grid>
     
   )
-	// <Form horizontal className="loginForm">
-  //   <FormGroup controlId="formHorizontalEmail">
-  //     <Col componentClass={ControlLabel} sm={2}>
-  //       Username
-  //     </Col>
-  //     <Col sm={10}>
-  //       <FormControl type="text" placeholder="Username" />
-  //     </Col>
-  //   </FormGroup>
-
-  //   <FormGroup controlId="formHorizontalPassword">
-  //     <Col componentClass={ControlLabel} sm={2}>
-  //       Password
-  //     </Col>
-  //     <Col sm={10}>
-  //       <FormControl type="password" placeholder="Password" />
-  //     </Col>
-  //   </FormGroup>
-
-  //   <FormGroup>
-  //     <Col smOffset={2} sm={10}>
-  //       <Checkbox>Remember me</Checkbox>
-  //     </Col>
-  //   </FormGroup>
-
-  //   <FormGroup>
-  //     <Col smOffset={2} sm={2}>
-  //       <Button bsStyle="primary" type="submit">Sign in</Button>
-  //     </Col>
-  //     <Col sm={2}>
-  //       <Link to="/sign-up">
-  //         <Button type="submit">Register</Button>
-  //       </Link>
-  //     </Col>
-  //   </FormGroup>
-  // </Form>
 }
 
 const mapStateToProps = state => ({
