fea74e092d34ed1134c3f777dd791833e9224edd
Brady Smith <bradsmi@siue.edu>
2019-03-18 13:34:53 -0500

    added a button for instructors who are new

--- src/components/Search-Components/NewUser-ModalComponent.jsx
+++ src/components/Search-Components/NewUser-ModalComponent.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import {Grid, Row} from 'react-bootstrap'
+import {Grid, Row, Button} from 'react-bootstrap'
 
 const NewUserModal = props => {
     return (
-        <Grid style = {{width:'100%'}}>
+        <Grid className = "User_Modal">
             <Row className = "User_Modal_Title">
                 Welcome to Contractor Connection, {props.user.name}!
+            </Row>
+            <Row className = "User_Modal_Subtext">
+                Are you an instructor? If so please click the following link!
+            </Row>
+            <Row className = "User_Modal_Buttons">
+                <Button>
+                    Instructor
+                </Button>
             </Row>
         </Grid>
     )
