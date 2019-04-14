479eb0faa4798af9d58ea9aa9f10e695c6dc3bb0
Billy Rohrkaste <williamrohrkaste@gmail.com>
2019-04-14 16:15:37 -0500

    updated quizzes

--- src/questions.js
+++ src/questions.js
@@ -1,13 +1,13 @@
-var quizData = [
+var quizData = [ 
     {
       questions: [
         "Question 1: How should LGBT people and minorities be treated when they show interest in being trained?",
-        "Question 2: Question 2",
-        "Question 3: q3",
-        "Question 4: q4",
-        "Question 5: q5",
+        "Question 2: What is the correct answer to this generic question?",
+        "Question 3: What is the correct answer to this generic question?",
+        "Question 4: What is the correct answer to this generic question?",
+        "Question 5: What is the correct answer to this generic question?",
       ],
-      video: "https://youtu.be/UW28j_V3dSI",
+      video: "https://youtu.be/H9DQeH8wTAM",
       correct: [
         3,0,0,0,0
       ],
@@ -16,135 +16,185 @@ var quizData = [
         "b. They should be treated with dignity",
         "c. They should be treated with compassion",
         "d. All of the above"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"]
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ]
       ] 
     },
     {
       questions: [
-        "Question 1",
-        "Question 2",
-        "Question 3",
-        "Question 4",
-        "Question 5",
+        "Question 1: What is the correct answer to this generic question?",
+        "Question 2: What is the correct answer to this generic question?",
+        "Question 3: What is the correct answer to this generic question?",
+        "Question 4: What is the correct answer to this generic question?",
+        "Question 5: What is the correct answer to this generic question?",
       ],
       video: "https://youtu.be/Z4EtsoYKQ0U",
       correct: [
         0,0,0,0,0
       ],
       answers: [
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"]
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ]
       ] 
     },
     {
       questions: [
-        "Question 1",
-        "Question 2",
-        "Question 3",
-        "Question 4",
-        "Question 5",
+        "Question 1: What is the correct answer to this generic question?",
+        "Question 2: What is the correct answer to this generic question?",
+        "Question 3: What is the correct answer to this generic question?",
+        "Question 4: What is the correct answer to this generic question?",
+        "Question 5: What is the correct answer to this generic question?",
+      ],
+      video: "https://youtu.be/UW28j_V3dSI",
+      correct: [
+        0,0,0,0,0
+      ],
+      answers: [
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ]
+      ] 
+    },
+    {
+      questions: [
+        "Question 1: What is the correct answer to this generic question?",
+        "Question 2: What is the correct answer to this generic question?",
+        "Question 3: What is the correct answer to this generic question?",
+        "Question 4: What is the correct answer to this generic question?",
+        "Question 5: What is the correct answer to this generic question?",
       ],
       video: "https://youtu.be/H9DQeH8wTAM",
       correct: [
         0,0,0,0,0
       ],
       answers: [
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"]
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ]
       ] 
     },
     {
       questions: [
-        "Question 1",
-        "Question 2",
-        "Question 3",
-        "Question 4",
-        "Question 5",
+        "Question 1: What is the correct answer to this generic question?",
+        "Question 2: What is the correct answer to this generic question?",
+        "Question 3: What is the correct answer to this generic question?",
+        "Question 4: What is the correct answer to this generic question?",
+        "Question 5: What is the correct answer to this generic question?",
       ],
       video: "https://youtu.be/H9DQeH8wTAM",
       correct: [
         0,0,0,0,0
       ],
       answers: [
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. They should be treated with respect",
+        "b. They should be treated with dignity",
+        "c. They should be treated with compassion",
+        "d. All of the above"],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"],
-        ["this",
-        "that",
-        "this and that",
-        "none"
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
         ],
-        ["this",
-        "that",
-        "this and that",
-        "none"]
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ],
+        ["a. correct",
+        "b. incorrect",
+        "c. incorrect",
+        "d. incorrect"
+        ]
       ] 
     }
   ];
